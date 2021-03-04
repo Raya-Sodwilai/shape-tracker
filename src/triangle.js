af227f667621a5a18b2efbb056f62d9a6ffd8131
Raya Sodwilai <sodwilai@pdx.edu>
2021-03-03 17:41:19 -0800

    jQuery and Bootstrap to the webpage

--- src/triangle.js
+++ src/triangle.js
@@ -1,5 +1,4 @@
-// Business Interface Logic -------
-function Triangle(side1, side2, side3) {
+export default function Triangle(side1, side2, side3) {
   this.side1 = side1;
   this.side2 = side2;
   this.side3 = side3;
@@ -7,4 +6,4 @@ function Triangle(side1, side2, side3) {
 
 Triangle.prototype.checkType = function() {
   return "I can't answer that yet!";
-}
+};
